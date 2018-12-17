02043de923cab113b0db8a9dddc6f6a66427130e
james727 <jameshkatz@gmail.com>
2018-12-17 11:16:58 -0500

    Extract heap log util into separate repository

---
+++ README.md
@@ -0,0 +1,2 @@
+# Heap Logs
+Utils for Heap logging. Extracted into a separate repository for easy installation via NPM.
---
+++ index.js
@@ -0,0 +1,75 @@
+var appInstance,
+conf,
+defaultLogger,
+facilityMap,
+hostname,
+logentries,
+logentriesToken,
+mainFilename,
+os,
+prod,
+Raygun,
+winston,
+_;
+
+_ = require('lodash');
+conf = require('config');
+fclone = require('fclone');
+os = require('os');
+Raygun = require('winston-raygun');
+winston = require('winston');
+require('winston-log-and-exit');
+
+/*
+TODO: This should be configured by an external file.
+It shouldn't require a code change to set logging prefs.
+*/
+winston.exitOnError = false;
+
+mainFilename = _.last(process.mainModule.filename.split('/'));
+hostname = os.hostname();
+
+var logger = new (winston.Logger)({
+rewriters: [
+  function(label, msg, meta) {
+    meta = fclone(meta); // Remove circular references.
+    meta.mainFilename = mainFilename;
+    meta.hostname = hostname;
+    return meta;
+  }
+]
+});
+
+prod = process.env.NODE_ENV === 'prod';
+logger.add(winston.transports.Console, {
+colorize: (process.env.LOG_COLOR || 'yes') !== 'no',
+timestamp: prod,
+level: process.env.LOG_LEVEL || 'info'
+});
+
+// In an effort to minimize noisiness, manually specify what gets logged to Raygun.
+raygunLog = Raygun.prototype.log;
+Raygun.prototype.log = function(level, msg, meta, callback){
+if (meta.logToRaygun) {
+  raygunLog.call(this, level, msg, meta, callback);
+}
+};
+
+if (prod) {
+logger.add(Raygun, {
+  apiKey: conf.raygun.backend
+});
+}
+
+if (prod) {
+var fluentConfig = {
+  host: 'localhost',
+  port: 24224,
+  timeout: 3.0
+};
+var fluentTransport = require('fluent-logger').support.winstonTransport();
+var transport = new fluentTransport('heap.coffee', fluentConfig);
+logger.add(transport, null, true);
+}
+
+module.exports = logger;
