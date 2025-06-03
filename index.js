131e9d12bf12b1ae24b70174a41d0d6b666b9eb7
Jack Szubert <jack.szubert@heapanalytics.com>
2025-06-03 07:07:36 +0000

    reset to the version used in heap/heap

--- index.js
+++ index.js
@@ -1,62 +1,54 @@
-import * as os from 'os';
-import * as path from 'path';
-
-import fclone from 'fclone';
-import * as winston from 'winston';
-
-import { MetadataRewriter } from 'winston';
-import { argv } from 'yargs';
-
+var fclone = require('fclone');
+var os = require('os');
+var path = require('path');
+var winston = require('winston');
+var argv = require('yargs').argv;
 require('winston-log-and-exit');
 
-const isProd = process.env.NODE_ENV === 'prod';
-const isECS = process.env.HEAP_ECS === 'true';
-const isK8s = !!process.env.POD_NAMESPACE;
-const isProdDeployment = isECS || isK8s;
-
-type HeapArgV = {
-  pm2path: string;
-};
+var isProd = process.env.NODE_ENV === 'prod';
+var isECS = process.env.HEAP_ECS == 'true';
 
 /*
 TODO: This should be configured by an external file.
 It shouldn't require a code change to set logging prefs.
 */
-(winston as any).exitOnError = false;
+winston.exitOnError = false;
 
 // process.mainModule is undefined when node reads from STDIN.
-let mainFilename =
-  process.mainModule === null ? 'unknown' : path.basename(process.mainModule.filename);
+var mainFilename = process.mainModule == null
+    ? 'unknown'
+    : path.basename(process.mainModule.filename);
 
 // :KLUDGE: Add special handling for our pm2 loading setup.
 if (mainFilename === 'pm2_loader.js') {
   try {
-    mainFilename = path.basename(((argv as any) as HeapArgV).pm2path);
-  } catch (e) {
+    mainFilename = path.basename(argv.pm2path)
+  }
+  catch (e) {
     // An error was encountered, likely because pm2path wasn't passed
     // in. In this case we'll just use pm2_loader for the mainfilename.
-  }
+  };
 }
 
-const hostname = os.hostname();
+var hostname = os.hostname();
 
-const rewriters: Array<MetadataRewriter> = [
-  (label, msg, meta) => {
+var rewriters = [
+  function(label, msg, meta) {
     meta = fclone(meta); // Remove circular references.
     meta.mainFilename = mainFilename;
     meta.hostname = hostname;
     return meta;
-  },
-];
+  }
+]
 
-if (isProdDeployment) {
-  rewriters.push((label, msg, meta) => {
+if (isECS) {
+  rewriters.push(function(label,msg, meta) {
     // First, check if the meta object is actually an error. In this case we want to preserve
     // the error message (which would otherwise be clobbered by the log message). Winston populates
     // the `name` field of the meta object with the class of the object if available, so just check
     // for `name === 'Error'` here.
     if (meta.name === 'Error') {
-      meta.error_message = meta.message;
+      meta.error_message = meta.message
       delete meta.message;
       delete meta.name; // This field just looks confusing.
       return meta;
@@ -65,27 +57,27 @@ if (isProdDeployment) {
     // Otherwise, we expect the meta object to be tags for the log line. Apply some rudimentary
     // sanitizing here to stringify unexpected nested objects. All of our tags should be either
     // string or number values.
-    for (const key of Object.keys(meta)) {
-      const value = meta[key];
-      const isString = typeof value === 'string';
-      const isNumber = typeof value === 'number';
+    for (var key of Object.keys(meta)) {
+      var value = meta[key];
+      var isString = typeof value === 'string';
+      var isNumber = typeof value === 'number';
       if (!isString && !isNumber) {
         meta[key] = '' + JSON.stringify(value);
       }
     }
 
     return meta;
-  });
+  })
 }
 
-const logger = new winston.Logger();
-logger.rewriters = rewriters;
+var logger = new (winston.Logger)({rewriters});
+
 logger.add(winston.transports.Console, {
-  json: isProdDeployment,
+  json: isECS,
   stringify: (obj) => JSON.stringify(obj),
-  colorize: !isProdDeployment,
+  colorize: !isECS,
   timestamp: isProd,
-  level: process.env.LOG_LEVEL || 'info',
+  level: process.env.LOG_LEVEL || 'info'
 });
 
 module.exports = logger;
